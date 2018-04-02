574896a1e75614ffc8fad786ef4403ef06787b7a
Radek <radek511@op.pl>
2018-04-02 15:08:30 +0200

    Styled header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import "../styles/header.css";
 
 class Header extends Component {
   render() {
     return (
-      <header>
-        <h1>Weather appliaction</h1>
+      <header className="weather-header">
+        <h1 className="weather-header-title">Weather<span>App</span></h1>
+        <div className="weather-header-language">
+          <p>Select language</p>
+          <select className="weather-header-language-select">
+            <option>En</option>
+            <option>Pl</option>
+          </select>
+        </div>
       </header>
     );
   }
