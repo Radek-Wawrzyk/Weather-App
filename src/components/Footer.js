83845e1cd893b9b09bde24dae3c054c57e823d93
Radek <radek511@op.pl>
2018-04-02 15:27:31 +0200

    Styled footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import "../styles/footer.css";
 
 class Footer extends Component {
   render() {
     return (
-      <footer>
-        <p>Site by <a href="https://github.com/Radek-Wawrzyk">Radziu</a></p>
+      <footer className="footer">
+        <p className="footer-author">Site by <a href="https://github.com/Radek-Wawrzyk">Radziu</a></p>
+        <div className="footer-socials">
+          <a href="https://github.com/Radek-Wawrzyk"><span className="fa fa-github" aria-hidden="true"></span></a>
+          <a href="https://www.linkedin.com/in/radek-wawrzyk-211948158/"><span className="fa fa-linkedin-square" aria-hidden="true"></span></a>
+          <a href="https://www.facebook.com/radek.wawrzyk"><span className="fa fa-facebook-square" aria-hidden="true"></span></a>
+        </div>
       </footer>
     );
   }
