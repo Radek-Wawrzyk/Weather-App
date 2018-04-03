ac01e9fdd65f27e62ce3d2c0111b2614fe135dbc
Radek <radek511@op.pl>
2018-04-03 15:32:10 +0200

    Refactored weather component code
    Also did some changes in css

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,48 +1,49 @@
 import React, { Component } from 'react';
 import "../styles/weather.css";
 
-class Weather extends Component {
+class Weather extends Component { 
   render() {
-    return (
-      <section className="weather" id="weather">
-        {  this.props.city &&
+    let component;
+    if (this.props.city) {
+      component = (
+        <section className="weather" id="weather">
           <div className="weather-tile">
             <p> City: <span>{this.props.city}</span></p> 
           </div>
-        }
-        { this.props.country &&
           <div className="weather-tile">
             <p>Country: <span>{this.props.country}</span></p> 
           </div>
-        }
-        { this.props.temperature &&
           <div className="weather-tile">
             <p>temperature: <span>{this.props.temperature}°C</span></p> 
           </div>
-        }
-        { this.props.description &&
           <div className="weather-tile">
             <p>Weather: <span>{this.props.description}</span></p>
           </div>
-        }
-        { this.props.wind &&
           <div className="weather-tile">
             <p> Wind speed: <span>{this.props.wind} Mph</span></p> 
           </div>
-        }
-        { this.props.humidity && 
           <div className="weather-tile">
             <p>Humidity: <span>{this.props.humidity}%</span></p>
           </div>
-        }
-        { this.props.error &&
           <div className="weather-tile weather-tile-error">
             <p> <span className="error">{this.props.error}</span></p>
           </div>  
-        }
-      </section>
+        </section> 
+      )
+    } else {
+      component = (
+        <p>{this.props.error}</p>
+      )
+    }
+    return (
+      <div>
+        {component}
+      </div>
     );
   }
 }
 
 export default Weather;
+
+
+      
