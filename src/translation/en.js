4491e01be917743e74f736671871b68e6d37f911
Radek <radek511@op.pl>
2018-04-04 17:14:44 +0200

    Changed json files for normal js

---
+++ src/translation/en.js
@@ -0,0 +1,19 @@
+const data = {
+  header: "Select language",
+  form: {
+    placeholder: "City",
+    button: "Get Weather"
+  },
+  weather: {
+    description: "Feels like",
+    windSpeed: "Wind speed",
+    humidity: "Humidity",
+    pressure: "Pressure",
+    visibility: "Visibility",
+    cloudy: "Cloudy"
+  },
+  error: "Enter City",
+  footer: "Site b "
+}
+
+export default data
---
+++ src/translation/pl.js
@@ -0,0 +1,19 @@
+const data = {
+  header: "Wybierz język",
+  form: {
+    placeholder: "Miasto",
+    button: "Sprawdź pogodę"
+  },
+  weather: {
+    description: "Wygląda na",
+    windSpeed: "Prędkość wiatru",
+    humidity: "Wilgotność",
+    pressure: "Ciśnienie",
+    visibility: "Widoczność",
+    cloudy: "zachmurzenie"
+  },
+  error: "Wpisz miasto",
+  footer: "Strony zrobiona przez" 
+}
+
+export default data
