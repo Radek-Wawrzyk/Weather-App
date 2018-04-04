fc26a18015e830823383b96564ae338220c5c734
Radek <radek511@op.pl>
2018-04-04 20:25:41 +0200

    Added change language feature
    Remodeled system of changing site language

---
+++ src/languages/Data.js
@@ -0,0 +1,38 @@
+const Data = [
+  {
+    header: "Wybierz język",
+    form: {
+      placeholder: "Miasto",
+      button: "Sprawdź pogodę"
+    },
+    weather: {
+      description: "Wygląda na",
+      windSpeed: "Prędkość wiatru",
+      humidity: "Wilgotność",
+      pressure: "Ciśnienie",
+      visibility: "Widoczność",
+      cloudy: "zachmurzenie"
+    },
+    error: "Wpisz miasto",
+    footer: "Strona stworzona przez  " 
+  },
+  {
+    header: "Select language",
+    form: {
+      placeholder: "City",
+      button: "Get Weather"
+    },
+    weather: {
+      description: "Feels like",
+      windSpeed: "Wind speed",
+      humidity: "Humidity",
+      pressure: "Pressure",
+      visibility: "Visibility",
+      cloudy: "Cloudy"
+    },
+    error: "Enter City",
+    footer: "Site by"
+  }
+];
+
+export default Data;
